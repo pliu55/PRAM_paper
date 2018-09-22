46013bfb126a7a05f19a37061977b22b08dc4fa6
Peng Liu <pliu55@wisc.edu>
2018-09-22 10:24:15 -0500

    add functions to evaluate models

--- 1_benchmark/run.R
+++ 1_benchmark/run.R
@@ -14,8 +14,10 @@
 #  ~23G
 #
 
-library(pram)
+library(data.table)
 library(parallel)
+library(pram)
+suppressMessages(library(rtracklayer))
 
 main <- function() {
     currdir = getwd()
@@ -28,25 +30,28 @@ main <- function() {
         methods = c(  'plcf', 'plst', 'cfmg', 'stmg', 'cftc' ),
 
         bamids = c(
-            'ENCFF044SJL', 'ENCFF048ODN', 'ENCFF125RAL', 'ENCFF207ZSA',
-            'ENCFF244ZQA', 'ENCFF263OLY', 'ENCFF306YQS', 'ENCFF315VHI',
-            'ENCFF343WEZ', 'ENCFF367VEP', 'ENCFF381BQZ', 'ENCFF428VBU',
-            'ENCFF444SCT', 'ENCFF521KYZ', 'ENCFF547YFO', 'ENCFF588YLF',
-            'ENCFF709IUX', 'ENCFF728JKQ', 'ENCFF739OVZ', 'ENCFF782IVX',
-            'ENCFF782TAX', 'ENCFF800YJR', 'ENCFF802TLC', 'ENCFF834ITU',
-            'ENCFF838JGD', 'ENCFF846WOV', 'ENCFF904OHO', 'ENCFF912SZP',
-            'ENCFF978ACT', 'ENCFF983FHE'
+            "ENCFF125RAL", "ENCFF739OVZ", "ENCFF802TLC", "ENCFF428VBU",
+            "ENCFF547YFO", "ENCFF782IVX", "ENCFF709IUX", "ENCFF244ZQA",
+            "ENCFF343WEZ", "ENCFF444SCT", "ENCFF315VHI", "ENCFF834ITU",
+            "ENCFF306YQS", "ENCFF521KYZ", "ENCFF800YJR", "ENCFF782TAX",
+            "ENCFF912SZP", "ENCFF207ZSA", "ENCFF846WOV", "ENCFF588YLF",
+            "ENCFF048ODN", "ENCFF381BQZ", "ENCFF044SJL", "ENCFF728JKQ",
+            "ENCFF367VEP", "ENCFF983FHE", "ENCFF904OHO", "ENCFF838JGD",
+            "ENCFF263OLY", "ENCFF978ACT"
         ),
 
         inbam_url = 'ftp://ftp.cs.wisc.edu/pub/users/kelesgroup/pliu/pram_paper/known/12_poolTgtBam/',
 
+        setupdir  = setupdir,
         cufflinks = paste0(setupdir, 'cufflinks-2.2.1.Linux_x86_64/cufflinks'),
         stringtie = paste0(setupdir, 'stringtie-1.3.3b.Linux_x86_64/stringtie'),
         taco      = paste0(setupdir, 'taco-v0.7.0.Linux_x86_64/taco_run'),
         fgnmfa    = paste0(setupdir, 'hg38_cufflinks.fa'),
 
-        indir  = paste0(currdir, '/input/'),
-        outdir = paste0(currdir, '/output/')
+        ftgtids = paste0(currdir, '/reported/target_transcript_ids.txt.gz'),
+        indir   = paste0(currdir, '/input/'),
+        outdir  = paste0(currdir, '/output/'),
+        fout    = paste0(currdir, '/output/model_eval.tsv')
     )
 
     for ( dir in c(prm$indir, prm$outdir) ) {
@@ -60,6 +65,33 @@ main <- function() {
     ## run pram
     mclapply(prm$methods, buildMetaModel, prm,
              mc.cores=prm$njob_in_para)
+
+    ## evaluate models
+    evaluateModels(prm)
+}
+
+
+evaluateModels <- function(prm) {
+    fgnc = paste0(prm$setupdir, '/hg38_exon.gtf')
+    tgt_trids = readLines(prm$ftgtids)
+    tgtdt = data.table(readGFF(fgnc))[transcript_id %in% tgt_trids]
+    setnames(tgtdt, 'seqid', 'chrom')
+
+    outdt = rbindlist(mclapply(prm$methods, evalMetaModel, tgtdt, prm$outdir,
+                               mc.cores=prm$njob_in_para))
+    write.table(outdt, prm$fout, quote=F, sep="\t", row.names=F)
+    cat('file written:', prm$fout, "\n")
+}
+
+
+evalMetaModel <- function(method, tgtdt, outdir) {
+    fmdl = paste0(outdir, method, '.gtf')
+    mdldt = data.table(readGFF(fmdl))
+    setnames(mdldt, 'seqid', 'chrom')
+    evaldt = pram::evalModel(mdldt, tgtdt)
+    evaldt[, method := method]
+
+    return(evaldt)
 }
 
 
